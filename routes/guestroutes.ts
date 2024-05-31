b60c2890902a71103bc3f04d949fcd3029dfc429
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-06-01 02:56:32 +0530

    jwt integrated

--- routes/guestroutes.ts
+++ routes/guestroutes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getAllGuests,searchAllGuests,getAdmin } from "../controllers/guestcontroller";
+import { getAllGuests,searchAllGuests,loginAdmin,addNewGuests,getReserv,addBooking,editBooking,getAvailableRooms,getThisRooms} from "../controllers/guestcontroller";
 const router = Router();
 
-router.get("/getGuests", getAllGuests)
-router.get("/searchAllGuest",searchAllGuests)
-router.get("/getAdmin",getAdmin);
+router.get("/getGuests", getAllGuests);
+router.get("/searchAllGuest",searchAllGuests);
+router.get("/getAdmin",loginAdmin);
+router.post("/addGuests",addNewGuests);
+router.get("/getReserv",getReserv);
+router.post("/addBooking",addBooking);
+router.post("/editBooking",editBooking);
+router.get("/getAvailableRooms",getAvailableRooms);
+router.get("/getThisRooms",getThisRooms);
 
 export default router;
