65eae1f749d0f6e811d7f6c9e2bd1fe3fe82409c
deepanshupal09 <101573843+deepanshupal09@users.noreply.github.com>
2024-11-10 18:09:52 +0530

    checkin bug fix

--- models/cosmodel.ts
+++ models/cosmodel.ts
@@ -577,7 +577,7 @@ export async function fetchCheckInByRoomModel(room: string) {
 export async function updateCheckinGuestModel(booking_id: string, document_url: string, email: string, room: string) {
   try {
     const res = await pool.query(updateCheckInGuestQuery,[document_url, email, booking_id]);
-    const old_email = res.rows[0].guest_email;
+    const old_email = res.rows[0].old_email;
     // await pool.query(upsertRestrictionUserQuery,[coupon_id_checkin,email,convertUTCToIST(new Date()),convertUTCToIST(new Date()),true]);
     const res2 = await pool.query(fetchCouponDetailsQuery,[coupon_id_checkin]);
     const coupon:Coupon = res2.rows[0];
