efaefeb8f98796a73ee8d3530fe340e8ca5d2d9a
deepanshupal09 <101573843+deepanshupal09@users.noreply.github.com>
2025-10-14 21:01:00 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -196,7 +196,7 @@ const s3 = new AWS.S3({
 const transporter = nodemailer.createTransport({
   // service: "gmail", // You can use any email service
   host: "smtp.mailgun.org",
-  port: 2525,
+  port: 465,
   auth: {
     user: process.env.NODE_MAIL_USER,
     pass: process.env.NODEMAILER_PASSWORD,
